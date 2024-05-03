130204723f4e52c4bb43cfe1cdc563598f1c3404
mapas <mapas@Nitro-Angel>
2024-05-03 18:41:10 +0200

    2ª Modificación en Nitro

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main() {
 		System.out.println ("Estamos haciendo control de versiones con Git");
 		System.out.println ("Estamos haciendo 2º control de versiones con Git");
+		System.out.println ("Estamos haciendo 3er control de versiones con Git");
 		
 	}
 }
