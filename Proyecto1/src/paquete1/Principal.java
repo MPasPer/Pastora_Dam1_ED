d303e24ca7bebd0b5eeb206cc52c621cadf89163
mapas <mapas@Nitro-Angel>
2024-05-03 18:38:02 +0200

    1ª modificación de la clase creada, Nitro

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main() {
 		System.out.println ("Estamos haciendo control de versiones con Git");
+		System.out.println ("Estamos haciendo 2º control de versiones con Git");
 		
 	}
 }
