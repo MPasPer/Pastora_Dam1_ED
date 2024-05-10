7c2fbe937b2348d6961cd5e094fda0678a88ddc1
Usuario <Usuario@PC-Miguel.fritz.box>
2024-05-11 00:13:33 +0200

    Validación de Principal10

---
+++ Proyecto10/src/paquete10/Principal10.java
@@ -0,0 +1,12 @@
+package paquete10;
+
+public class Principal10 {
+
+	public static void main() {
+		System.out.println ("Estamos haciendo control de versiones con Git");
+		System.out.println ("Estamos haciendo 2º control de versiones con Git");
+		System.out.println ("Estamos haciendo 3er control de versiones con Git");
+		System.out.println ("Estamos haciendo 4º control de versiones con Git");
+		
+	}
+}
