c2f70bab9d95c844979e9abe4b2ba14a56020b3f
Usuario <Usuario@PC-Miguel.fritz.box>
2024-05-11 12:12:37 +0200

    Subida de la rama 10 al repositorio local

--- Proyecto10/src/paqueteRama10/Rama10.java
+++ Proyecto10/src/paqueteRama10/Rama10.java
@@ -3,7 +3,7 @@ package paqueteRama10;
 public class Rama10 {
 
 	public static void main() {
-		System.out.println ("Estamos creando una rama y validandola");
+		System.out.println ("Estamos creando una rama con una nueva clase y validandola");
 	}
 
 }
